2f0c39d3862e5c27fd14118221686c777747c85e
cedrovsk <89600920+tcedro@users.noreply.github.com>
2022-07-05 21:10:29 -0300

    Update contaBancaria.cpp

--- c++/contaBancaria/contaBancaria.cpp
+++ c++/contaBancaria/contaBancaria.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 class Conta {
 
@@ -22,6 +23,8 @@ class Conta {
 
         void depositarValor() {
             double deposito = 0.0;
+            int escolha = 0;
+
             std::cout << "Valor a ser depositado: " << std::endl << std::endl;
             std::cin >> deposito;
             if (deposito <= 0) {
@@ -31,11 +34,23 @@ class Conta {
                 std::cout << "Deposito concluido!" << std::endl << std::endl;
                 this->saldo += deposito;
             }
+
+            do {
+                std::cout << "Deseja emitir comprovante: " << std::endl << "1- SIM | 2-NAO" << std::endl;
+                std::cin >> escolha;
+                
+            } while (escolha > 1 && escolha < 0);
+
+            if (escolha == 1) {
+                emitirComprovanteDeposito(deposito);
+            } 
+
            
         } // fim do depositarValor
 
         void sacarValor() {
             double saque = 0;
+            int escolha = 0;
             
             std::cout << "Valor a ser sacado: " << std::endl << std::endl;
             std::cin >> saque;
@@ -47,6 +62,17 @@ class Conta {
                 this->saldo -= saque;
                 std::cout << "Saque efetuado: " << this->nameUsuario << std::endl << std:: endl;
             }
+
+            do {
+                std::cout << "Deseja emitir comprovante: " << std::endl << "1- SIM | 2-NAO" << std::endl;
+                std::cin >> escolha;
+            
+            } while (escolha > 1 && escolha < 0);
+
+            if (escolha == 1) {
+                emitirComprovanteSaque(saque);
+            } 
+
         } //fim do sacarValor 
 
         void mostrarSaldo() {
@@ -54,9 +80,9 @@ class Conta {
         }
 
         void transferirValor(Conta *link2) {
-
             double transferencia;
-            
+            int escolha = 0;
+
             std::cout << "Valor da tranferencia: " << std::endl << std::endl;
             std::cin >> transferencia;
 
@@ -73,8 +99,43 @@ class Conta {
                     link2->saldo += transferencia;
                 }
             }
+
+            do {
+                std::cout << "Deseja emitir comprovante: " << std::endl << "1- SIM | 2-NAO" << std::endl;
+                std::cin >>  escolha;
             
+            } while (escolha > 1 && escolha < 0);
+            
+
+            if (escolha == 1) {
+                emitirComprovanteTrasnferencia(transferencia);
+            } 
+
         }// fim do TranferirValor
+
+        void emitirComprovanteSaque(int saque)  {
+            std::ofstream myfile;
+            
+            myfile.open ("Comprovante de Saque.txt");
+            myfile << "Comprovante de saque: " << std::endl << this->nameUsuario << std::endl << "===============================================" <<  std::endl << "- " << saque << std::endl;
+            myfile.close();
+        }
+
+        void emitirComprovanteDeposito(int deposito)  {
+            std::ofstream myfile;
+            
+            myfile.open ("Comprovante de deposito.txt");
+            myfile << "Comprovante de deposito: " << std::endl << this->nameUsuario << std::endl << "===============================================" <<  std::endl << "+ " << deposito << std::endl;
+            myfile.close();
+        }
+
+        void emitirComprovanteTrasnferencia(int transferencia)  {
+            std::ofstream myfile;
+            
+            myfile.open ("Comprovante de tranferencia.txt");
+            myfile << "Comprovante de transferencia: " << std::endl << this->nameUsuario << std::endl << "===============================================" <<  std::endl << transferencia << std::endl;
+            myfile.close();
+        }
 
 };
 
@@ -90,7 +151,8 @@ void menuBancario(Conta *link, Conta *link2) {
         std::cout <<  "4- Transferir" << std::endl << std::endl;
         std::cout <<  "0- SAIR" << std::endl << std::endl;
         std::cin >> opcao;
-
+        std::cout << "===============================================" << std::endl << std::endl;
+        
         std::cout << std::endl << std::endl;
 
         switch (opcao) {
